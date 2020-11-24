a5a40208f3be9c974c250c42f5a9636397c5cd0d
Nils Lundell <Nils.Lundell@mardigras.se>
2020-11-24 09:35:54 +0100

    Added more test cases in FIzzBuzz test

--- FizzBuzzTest/FizzBuzzTest.cs
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -18,11 +18,19 @@ namespace FizzBuzzTest
             var res3 = p.FizzBuzz(15);
             var res4 = p.FizzBuzz(42);
 
+            var res5 = p.FizzBuzz(18);
+            var res6 = p.FizzBuzz(25);
+            var res7 = p.FizzBuzz(90);
+
             //Assert
             Assert.AreEqual("Fizz", res1);
             Assert.AreEqual("Buzz", res2);
             Assert.AreEqual("FizzBuzz", res3);
             Assert.AreEqual("Answer to the Ultimate Question of Life, the Universe, and Everything", res4);
+
+            Assert.AreEqual("Fizz", res5);
+            Assert.AreEqual("Buzz", res6);
+            Assert.AreEqual("FizzBuzz", res7);
         }
     }
 }
